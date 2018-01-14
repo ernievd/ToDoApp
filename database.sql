ffea43493c63ab76d37a38369506547205b18566
ernievd <ernievd@gmail.com>
2018-01-14 08:51:26 -0600

    initial commit

--- database.sql
+++ database.sql
@@ -7,7 +7,5 @@ CREATE TABLE koalas (
 	notes VARCHAR(255) DEFAULT 'None'
 );
 
-
-
 INSERT INTO koalas (name, gender, age, ready_to_transfer, notes) 
 VALUES ('Scotty', 'M', 4, 'Y', 'Born in Guatemala'), ('Jean', 'F', 5, 'Y', 'Allergic to lots of lava'), ('Ororo', 'F', 7, 'N', 'Loves listening to Paula(Abdul)'), ('Logan', 'M', 15, 'N', 'Loves the sauna'), ('Charlie', 'M', 9, 'Y', 'Favorite band is Nirvana'), ('Betsy', 'F', 4, 'Y', 'Has a pet iguana');
